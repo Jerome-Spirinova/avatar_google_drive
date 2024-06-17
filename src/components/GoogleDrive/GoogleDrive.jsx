471c82688c60e76fadade0c15ae4d28ab16e02b2
Jerome-Spirinova <jerome@spirinova.dev>
2024-06-17 16:40:13 +0530

    revert

--- src/components/GoogleDrive/GoogleDrive.jsx
+++ src/components/GoogleDrive/GoogleDrive.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Login from "./Login";
 import "../../style.css";
 
 export const GoogleDrive = () => {
+  const [accessToken, setAccessToken] = useState("");
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    if (accessToken) {
+      fetchFiles();
+    }
+  }, [accessToken]);
+
+  const fetchFiles = async () => {
+    try {
+      const response = await fetch("https://www.googleapis.com/drive/v3/files", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        }
+      });
+      const data = await response.json();
+      setFiles(data.files);
+    } catch (error) {
+      console.error("Error fetching files: ", error);
+    }
+  };
 
   return (
-    <>
+    <GoogleOAuthProvider clientId="375499242890-r9i0i6cli22p4f5q0cv8dss48k4od2s1.apps.googleusercontent.com">
       <h2>React Google Drive Integration</h2>
-    </>
+      <Login />
+      {files.length > 0 && (
+        <div>
+          <h3>Files:</h3>
+          <ul>
+            {files.map((file) => (
+              <li key={file.id}>{file.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </GoogleOAuthProvider>
   );
 };
--- src/components/GoogleDrive/Login.jsx
+++ src/components/GoogleDrive/Login.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import { useGoogleLogin } from '@react-oauth/google';
+import React from "react";
+import { useGoogleLogin } from "@react-oauth/google";
 
 const Login = () => {
+  const authorize = useGoogleLogin({
+    onSuccess: (response) => {
+      console.log(response);
+    },
+    flow: "auth-code",
+    scope: "https://www.googleapis.com/auth/drive",
+  });
 
-    const authorize = useGoogleLogin({
-        onSuccess: (response) => {
-          console.log(response);
-        },
-        flow: "auth-code",
-        scope: "https://www.googleapis.com/auth/drive",
-      });
+  return <button onClick={() => authorize()}>Login</button>;
+};
 
-  return (
-    <button>Login</button>
-  )
-}
-
-export default Login
+export default Login;
