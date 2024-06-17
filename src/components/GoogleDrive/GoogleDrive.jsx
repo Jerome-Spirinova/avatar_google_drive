ec6e74225f8761f572006117051c142fa12c1bd0
Jerome-Spirinova <jerome@spirinova.dev>
2024-06-17 16:19:46 +0530

    login seprated

--- src/components/GoogleDrive/GoogleDrive.jsx
+++ src/components/GoogleDrive/GoogleDrive.jsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import Login from "./Login";
 import "../../style.css";
 
 export const GoogleDrive = () => {
   const [accessToken, setAccessToken] = useState("");
   const [files, setFiles] = useState([]);
-
-  const authorize = useGoogleLogin({
-    onSuccess: (response) => {
-      console.log(response);
-    },
-    flow: "auth-code",
-    scope: "https://www.googleapis.com/auth/drive",
-  });
 
   useEffect(() => {
     if (accessToken) {
@@ -37,7 +30,7 @@ export const GoogleDrive = () => {
   return (
     <GoogleOAuthProvider clientId="375499242890-r9i0i6cli22p4f5q0cv8dss48k4od2s1.apps.googleusercontent.com">
       <h2>React Google Drive Integration</h2>
-      <button onClick={() => authorize()}>Sign in with Google 🚀</button>
+      <Login />
       {files.length > 0 && (
         <div>
           <h3>Files:</h3>
---
+++ src/components/GoogleDrive/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useGoogleLogin } from '@react-oauth/google';
+
+const Login = () => {
+
+    const authorize = useGoogleLogin({
+        onSuccess: (response) => {
+          console.log(response);
+        },
+        flow: "auth-code",
+        scope: "https://www.googleapis.com/auth/drive",
+      });
+
+  return (
+    <button>Login</button>
+  )
+}
+
+export default Login
